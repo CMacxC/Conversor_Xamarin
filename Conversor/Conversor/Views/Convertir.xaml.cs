4ceb1d52a27130b1077d5e3308089e6291d46c6a
Mario A. <mac132@live.com.mx>
2023-10-10 13:21:16 -0600

    [ADD] Xamarin Conversor

---
+++ Conversor/Conversor/Views/Convertir.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Convertir : ContentPage
+    {
+        private double cm;
+        private double m;
+
+        public Convertir()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Volver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void Calcular()
+        {
+            if(!string.IsNullOrEmpty(txt_Numero.Text))
+            {
+                cm = Convert.ToDouble(txt_Numero.Text);
+                m = cm / 100;
+                lbl_Resultado.Text = m.ToString() + " m";
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese una cantidad.", "OK");
+            }
+        }
+
+        private void btn_Calcular_Clicked(object sender, EventArgs e)
+        {
+            Calcular();
+        }
+    }
+}
